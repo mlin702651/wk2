758e52171c0dea374ac1ac94b08840467ad3cc5f
Ding <mlin702651@gmail.com>
2020-03-16 19:31:05 +0800

    init

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+import Header from "./assets/Header";
+import Content from "./assets/Content";
+
+const App = () => {
+  return (
+    
+     <View>
+      <Header />
+      <Content />
+    </View>
+  );
+};
+export default App;
